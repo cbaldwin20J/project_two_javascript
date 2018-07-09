b9793084fc2d245266cdcb4ca352c26352ec1824
Chris Baldwin <cbaldwin20@gmail.com>
2018-07-08 20:41:04 -0500

    Got the search to restore pagination when search is empty. Now need to finish pagination for the search results.

--- js/script.js
+++ js/script.js
@@ -19,10 +19,23 @@ function paginate(num) {
 	$('#ul_paginate li a').eq(the_num).addClass('active');
 };
 
+function search_index(num) {
+	let count = 0;
+	let start = (num -1) * 10;
+	while (count < 10) {
+		$('li:visible').eq(start).show();
+		count += 1;
+		start += 1;
+	};
+	the_num = num - 1;
+	$('#ul_paginate li a.active').removeClass('active');
+	$('#ul_paginate li a').eq(the_num).addClass('active');
+}
 
 function start() {
 
 	$('ul li').hide();
+	$('#ul_paginate li').remove();
 	let $amount_of_lis = $('ul li').length;
 	let num_of_buttons = Math.ceil($amount_of_lis/10);
 
@@ -40,19 +53,25 @@ function start() {
 
 $('.student-search').on('click', 'button', function(){
 	let $input_val = $('.student-search input').val();
+	console.log($input_val);
+	if (!$input_val){
+		start();
+		return;
+	}
 
 	$('.student-list li').hide();
 	$('.student-list p').remove();
+	$('.student-list li.search').removeClass('search');
 
 	
 	let ifNone = 0;
 	$('.student-list li').each(function() {
 		let $str = $("h3", this).text();
 		let $iftrue = $str.search($input_val);
-		
+		let ifNone = 0;
 
 		if ($iftrue > -1) {
-			$(this).show();
+			$(this).addClass('search');
 			ifNone += 1;
 		};
 	});
@@ -62,9 +81,13 @@ $('.student-search').on('click', 'button', function(){
 		$('.student-list').append(
 			'<p>There were no matching results ' +
 			'for: ' + $input_val + ".</p>");
-	};
+	}else {
+		search_index(1);
+	
+	}
 
-
+	$('#ul_paginate li a.active').removeClass('active');
+	$('#ul_paginate li a').eq(0).addClass('active');
 
 
 
